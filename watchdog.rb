404b2e77bf687f228732bcaf5aa84a9c7532675c
Saul <october.sunbathe@gmail.com>
2017-03-18 17:31:02 +0800

    现在改成由程序管理定时任务, 同事调整了代码结构, 使之更加清晰.

--- watchdog.rb
+++ watchdog.rb
@@ -3,123 +3,157 @@ require "open-uri"
 require "nokogiri"
 require "pony"
 require "yaml"
+require "colorize"
+require "rufus-scheduler"
 
-TMP = "tmp"
-RULES = "rules"
 
-Dir.mkdir(TMP) unless File::directory?(TMP)
+class Watchdog
+  class << self
+    @@DIR_DATA = "data"
+    @@DIR_RULES = "rules"
+    @@FILE_USERS = "users.yaml"
 
-$load_success = true
-$rules = {}
+    @@UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
 
-puts "Load rules..."
-Dir.glob("#{RULES}/*.yaml").each do |file|
-  id = file[RULES.size+1 ... file.size-5].to_sym # 5 means ".yaml".size
-  data =  YAML.load_file(file)
-  error = false
-  ["name", "url", "css_selectors"].each do |key|
-    if data[key].nil? || data[key].empty?
-      $load_success = false
-      error = true
-      puts "  - [#{id}] FAILURE. '#{key}' not config."
-    end
-  end
-  unless error
-    $rules[id] = data.inject({}){|memo, (k,v)| memo[k.to_sym] = v; memo}.merge({:observers => []})
-    puts "  - [#{id}] success."
-  end
-end
+    @@logger = Logger.new("check.log")
+    @@logger.datetime_format = "%Y-%m-%d %H:%M:%S"
 
-puts "\nLoad users..."
-data = YAML.load_file("users.yaml")
-if data.is_a?(Array)
-  data.each do |user|
-    email = user["email"]
-    puts "  - for user: #{email}"
-    user["following"].each do |rule_id|
-      if $rules.key?(rule_id.to_sym)
-        $rules[rule_id.to_sym][:observers] << email
-        puts "    - [#{rule_id}] success."
+    @@RULES = {}
+
+    def run
+      Dir.mkdir(@@DIR_DATA) unless File::directory?(@@DIR_DATA)
+
+      if load_rules? && load_users?
+        puts "RUNNING...".green
+        start_scheduler
       else
-        $load_success = false
-        puts "    - [#{rule_id}] FAILURE. (Legal rules: #{$rules.keys})"
+        puts "FOUND ERROR. Abort.".red
       end
     end
-  end
-else
-  puts "  - FAILURE. no users."
-  $load_success = false
-end
 
-
-unless $load_success
-  puts "\nFOUNT ERROR. Abort."
-  exit
-end
-
-
-def modify_relative_link(scheme, host, attr)
-  if attr.value[0] == '/'
-    if attr.value[1] == '/'
-      attr.value = "#{scheme}:#{attr.value}"
-    else
-      attr.value = "#{scheme}://#{host}#{attr.value}"
+    private
+    def load_rules?
+      rt = true
+      puts "Load rules...".yellow
+      Dir.glob("#{@@DIR_RULES}/*.yaml").each do |file|
+        flag = true
+        id = file[@@DIR_RULES.size+1 ... file.size-5].to_sym # 5 means ".yaml".size
+        data = YAML.load_file(file)
+        ["name", "url", "css_selectors", "cron"].each do |key|
+          if data[key].nil? || data[key].empty?
+            puts "  FAILURE: [#{id}] '#{key}' not config".red
+            flag = false
+          end
+        end
+        if flag
+          puts "  SUCCESS: [#{id}]".green
+          @@RULES[id] = data.inject({}){|memo, (k,v)| memo[k.to_sym] = v; memo}.merge({:observers => []})
+        else
+          rt = false
+        end
+      end
+      puts "DONE.\n".yellow
+      rt
     end
+
+    def load_users?
+      rt = true
+      puts "Load users...".yellow
+      data = YAML.load_file(@@FILE_USERS)
+      if data.is_a?(Array)
+        data.each do |user|
+          email = user["email"]
+          puts "  for #{email}:"
+          user["following"].each do |rule_id|
+            if @@RULES.key?(rule_id.to_sym)
+              @@RULES[rule_id.to_sym][:observers] << email
+              puts "    SUCCESS: [#{rule_id}]".green
+            else
+              rt = false
+              puts "    FAILURE: [#{rule_id}] (Legal rules: #{@@RULES.keys})".red
+            end
+          end
+        end
+      else
+        rt = false
+        puts "  FAILURE: no users".red
+      end
+      puts "DONE.\n".yellow
+      rt
+    end
+
+    def start_scheduler
+      scheduler = Rufus::Scheduler.new
+
+      @@RULES.each do |k, v|
+        next if v[:observers].empty?
+        scheduler.cron v[:cron] do
+          check(k)
+        end
+      end
+
+      scheduler.join
+    end
+
+    def check(k)
+      v = @@RULES[k]
+
+      file_name = File.join(@@DIR_DATA, k.to_s)
+
+      old = File.read(file_name) if File.exist?(file_name)
+      new = Nokogiri::HTML(open(v[:url], {"User-Agent" => @@UA}).read).css(v[:css_selectors])
+
+      if new.to_s == old
+        @@logger.info "[#{k}] No updates."
+        return
+      end
+
+      @@logger.info "[#{k}] Found new."
+      # save to tmp file
+      File.open(file_name, "w") do |file|
+        file.print new.to_s
+      end
+
+      # handle relative path
+      uri = URI(v[:url])
+      new.each {|element| loop_element(uri.scheme, uri.host, element)}
+
+      # notify
+      v[:observers].each do |to|
+        Pony.mail(:to => to,
+                  :subject => "[订阅]#{v[:name]}",
+                  :html_body => "<html>#{new}</html>",
+                  :from => 'noreply@example.com', # you might change
+                  :via => :sendmail               # you might change
+                 )
+      end
+    end
+
+    def modify_relative_link(scheme, host, attr)
+      if attr.value[0] == '/'
+        if attr.value[1] == '/'
+          attr.value = "#{scheme}:#{attr.value}"
+        else
+          attr.value = "#{scheme}://#{host}#{attr.value}"
+        end
+      end
+    end
+
+    def loop_element(scheme, host, element)
+      itself = element.itself
+      case itself.name
+      when "img"
+        modify_relative_link(scheme, host, itself.attributes["src"])
+      when "a"
+        modify_relative_link(scheme, host, itself.attributes["href"])
+      end
+
+      element.children.each do |child|
+        loop_element(scheme, host, child) if element.is_a?(Nokogiri::XML::Element)
+      end
+    end
+
   end
 end
 
-def loop_element(scheme, host, element)
-  itself = element.itself
-  case itself.name
-  when "img"
-    modify_relative_link(scheme, host, itself.attributes["src"])
-  when "a"
-    modify_relative_link(scheme, host, itself.attributes["href"])
-  end
-
-  element.children.each do |child|
-    loop_element(scheme, host, child) if element.is_a?(Nokogiri::XML::Element)
-  end
-end
-
-UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
-
-puts "\nCheck for updates..."
-$rules.each do |k, v|
-  print "  - check [#{k}]."
-  if v[:observers].empty?
-    puts " SKIP <no followers>."
-    next
-  end
-
-  file_name = File.join(TMP, k.to_s)
-
-  old = File.read(file_name) if File.exist?(file_name)
-  new = Nokogiri::HTML(open(v[:url], {"User-Agent" => UA}).read).css(v[:css_selectors])
-
-  if new.to_s == old
-    puts " SKIP <no updates>."
-    next
-  end
-
-  puts " FOUND."
-  # save to tmp file
-  File.open(file_name, "w") do |file|
-    file.print new.to_s
-  end
-
-  # handle relative path
-  uri = URI(v[:url])
-  new.each {|element| loop_element(uri.scheme, uri.host, element)}
-
-  # notify
-  v[:observers].each do |to|
-    puts "    - send mail to <#{to}>"
-    Pony.mail(:to => to,
-              :subject => "[订阅]#{v[:name]}",
-              :html_body => "<html>#{new}</html>",
-              :from => 'noreply@example.com', # you might change
-              :via => :sendmail               # you might change
-             )
-  end
-end
+Watchdog.run
