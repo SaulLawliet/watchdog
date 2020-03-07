4e13cadb96f91a17ed165aa26d5094bbd920b64a
Saul Lawliet <october.sunbathe@gmail.com>
2020-03-07 20:18:42 +0800

    bilibili-up: 移除目标的关注数

--- fetchers/bilibili-up.rb
+++ fetchers/bilibili-up.rb
@@ -7,6 +7,7 @@ relation_stat = JSON.parse(open("https://api.bilibili.com/x/relation/stat?vmid=#
 space_upstat = JSON.parse(open("https://api.bilibili.com/x/space/upstat?mid=#{options.mid}").read)
 
 data = relation_stat["data"]
+data.delete("following")
 data["archive-view"] = space_upstat["data"]["archive"]["view"]
 data["article-view"] = space_upstat["data"]["article"]["view"]
 data["likes"] = space_upstat["data"]["likes"]
