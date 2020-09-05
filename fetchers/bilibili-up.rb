fcc16a8f9b72988f8d2bbf5cdf7142b7109e6d07
SaulLawliet <october.sunbathe@gmail.com>
2020-09-05 16:44:33 +0800

    bilibili需登陆才能获得赞统计，所以先去掉

--- fetchers/bilibili-up.rb
+++ fetchers/bilibili-up.rb
@@ -4,13 +4,14 @@ require 'open-uri'
 require 'json'
 
 relation_stat = JSON.parse(open("https://api.bilibili.com/x/relation/stat?vmid=#{options.mid}").read)
-space_upstat = JSON.parse(open("https://api.bilibili.com/x/space/upstat?mid=#{options.mid}").read)
+# 获赞数需要登陆, 暂时忽略
+# space_upstat = JSON.parse(open("https://api.bilibili.com/x/space/upstat?mid=#{options.mid}").read)
 
-data = relation_stat["data"]
-data.delete("following")
+data = {}
+data["follower"] = relation_stat["data"]["follower"]
 # 由于播放量和阅读量是日更, 所以不关心
 # data["archive-view"] = space_upstat["data"]["archive"]["view"]
 # data["article-view"] = space_upstat["data"]["article"]["view"]
-data["likes"] = space_upstat["data"]["likes"]
+# data["likes"] = space_upstat["data"]["likes"]
 
 JSON.pretty_generate(data)
