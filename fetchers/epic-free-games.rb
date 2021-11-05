d616b0db7395724e68271ffdd39d02fcf265bca6
SaulLawliet <october.sunbathe@gmail.com>
2021-11-05 13:37:33 +0800

    Yep, Fabian was right on this one.

--- fetchers/epic-free-games.rb
+++ fetchers/epic-free-games.rb
@@ -1,7 +1,7 @@
 require 'open-uri'
 require 'json'
 
-json = JSON.parse(open("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=US&allowCountries=US,CN").read)
+json = JSON.parse(URI.open("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?locale=en-US&country=US&allowCountries=US,CN").read)
 
 data = []
 json["data"]["Catalog"]["searchStore"]["elements"].each do |element|
@@ -12,7 +12,7 @@ json["data"]["Catalog"]["searchStore"]["elements"].each do |element|
       if promotionalOffer["discountSetting"]["discountPercentage"] == 0
         data << {
           "title" => element["title"],
-          "url" => "https://www.epicgames.com/store/product/" + element["productSlug"],
+          "url" => "https://www.epicgames.com/store/product/#{element["productSlug"]}",
           "startDate" => promotionalOffer["startDate"],
           "endDate" =>  promotionalOffer["endDate"]
         }
